283243347c896a6b72e0b7947af3c6066116033b
Digambaran <arjunsreekumar.1@gmail.com>
2023-12-14 22:04:53 +0530

    some changes

--- day_06/src/main.rs
+++ day_06/src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     fs::File,
-    io::{BufRead, BufReader, Lines, self},
+    io::{self, BufRead, BufReader, Lines},
     path::Path,
 };
 
@@ -8,20 +8,24 @@ use crate::{first::first_answer, second::second_answer};
 
 pub mod first;
 pub mod second;
+
 fn main() {
     let input_path = Path::new("./input.txt");
-    // println!("{}",input_path.to_str().unwrap());
-    if let Ok(mut lines) =  read_lines(input_path) {
-        while let Some(line)  = lines.next()  {
-           println!("{}",line.unwrap()); 
+    println!("{}", input_path.to_str().unwrap());
+    let lines_buff = read_lines(input_path);
+    match lines_buff {
+        Ok(mut lines) => {
+            while let Some(line) = lines.next() {
+                println!("{}", line.unwrap());
+            }
         }
-    }else {
-        println!("Error reading from file");
-        panic!("Failed");
+        Err(err) => {
+            println!("{:?}", err);
+        }
     }
     let first_answer: u128 = first_answer();
     let second_answer: u128 = second_answer();
-    println!("{}{}",first_answer,second_answer);
+    println!("{}{}", first_answer, second_answer);
 }
 
 fn read_lines<P>(filename: P) -> Result<Lines<BufReader<File>>, io::Error>
